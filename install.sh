9c00848878000631c3c4815c3f782aeabe0e9110
Ludovic Zipsin <ludovic.j.r.zipsin@gmail.com>
2016-02-28 04:10:47 +0100

    adding install instruction and script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ "$UID" -ne "$ROOT_UID" ];
+	then
+	echo ""
+	echo "Need to be root to launch this script. Please insert your root password below (nothing on screen):"
+	echo ""
+	sudo DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS "./`basename $0`"
+	exit
+fi
+
+for i in `ls .`; do
+    if [ -d $i ]; then
+        ./$i/install.sh
+    fi
+done
---
+++ repo-backup/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# carefull, should be root to use it
+
+rm -f /usr/local/bin/repo-backup
+chmod +x repo-backup.py
+cp repo-backup.py /usr/local/bin/repo-backup
